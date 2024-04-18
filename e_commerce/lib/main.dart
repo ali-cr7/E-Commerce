476184e00ebf7eace8092ae47be43352401aded1
ali-cr7 <alily4949@gmail.com>
2024-04-18 12:01:10 +0300

    firebase setup

--- e_commerce/lib/main.dart
+++ e_commerce/lib/main.dart
@@ -1,8 +1,20 @@
+import 'dart:io';
+
 import 'package:e_commerce/e_commerce.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+          apiKey: 'AIzaSyBABDd6uuYSQCmJW8jSMR4DGEwmmizpdow',
+          appId: '1:383833302617:android:96a65b6949a15483133348',
+          messagingSenderId: '383833302617',
+          projectId: 'e-commerce-715c0',
+        ))
+      : await Firebase.initializeApp();
+
   runApp(const ECommerce());
 }
-
-
